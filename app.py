29eef1c5a10829942f572aab96a86868b604b12e
HyunwooKim <hyunwoo.developer@gmail.com>
2022-01-10 19:20:27 +0900

    rename db

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ secret_for_jwt = 'SPARTA'
 
 # client = MongoClient(SECRET_KEY, 27017, authSource="admin")
 client = MongoClient('localhost', 27017)
-db = client.dbusers
+db = client.zzalFactory
 
 @app.route('/')
 def home():
@@ -81,4 +81,4 @@ def page_not_found(error):
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=5000, debug=True)
+    app.run("0.0.0.0", port=8000, debug=True)
--- static/js/register.js
+++ static/js/register.js
@@ -5,28 +5,28 @@ function register() {
     let password2 = $("#confirm_password2").val()
 
 
-//    if (password == "") {
-//        $("#help-password").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
-//        $("#input-password").focus()
-//        return;
-//    } else if (!is_password(password)) {
-//        $("#help-password").text("비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자").removeClass("is-safe").addClass("is-danger")
-//        $("#input-password").focus()
-//        return
-//    } else {
-//        $("#help-password").text("사용할 수 있는 비밀번호입니다.").removeClass("is-danger").addClass("is-success")
-//    }
-//    if (password2 == "") {
-//        $("#help-password2").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
-//        $("#input-password2").focus()
-//        return;
-//    } else if (password2 != password) {
-//        $("#help-password2").text("비밀번호가 일치하지 않습니다.").removeClass("is-safe").addClass("is-danger")
-//        $("#input-password2").focus()
-//        return;
-//    } else {
-//        $("#help-password2").text("비밀번호가 일치합니다.").removeClass("is-danger").addClass("is-success")
-//    }
+   // if (password == "") {
+   //     $("#help-password").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+   //     $("#input-password").focus()
+   //     return;
+   // } else if (!is_password(password)) {
+   //     $("#help-password").text("비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자").removeClass("is-safe").addClass("is-danger")
+   //     $("#input-password").focus()
+   //     return
+   // } else {
+   //     $("#help-password").text("사용할 수 있는 비밀번호입니다.").removeClass("is-danger").addClass("is-success")
+   // }
+   // if (password2 == "") {
+   //     $("#help-password2").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+   //     $("#input-password2").focus()
+   //     return;
+   // } else if (password2 != password) {
+   //     $("#help-password2").text("비밀번호가 일치하지 않습니다.").removeClass("is-safe").addClass("is-danger")
+   //     $("#input-password2").focus()
+   //     return;
+   // } else {
+   //     $("#help-password2").text("비밀번호가 일치합니다.").removeClass("is-danger").addClass("is-success")
+   // }
     $.ajax({
         type: "POST",
         url: "/register",
