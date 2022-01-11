10a7975d5994c067aae788ee7cd655ebc3b38dd2
Dzeko <vivala0519@naver.com>
2022-01-11 16:54:50 +0900

    update register

--- static/js/register.js
+++ static/js/register.js
@@ -2,31 +2,61 @@ function register() {
     let id = $("#input_id").val()
     let nickname = $("#nickname").val()
     let password = $("#input_password").val()
-    let password2 = $("#confirm_password2").val()
+    let password2 = $("#confirm_password").val()
+    let checkPassword = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,50}$/;
+    let checkEmail = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
 
-    console.log("a")
-   // if (password == "") {
-   //     $("#help-password").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
-   //     $("#input-password").focus()
-   //     return;
-   // } else if (!is_password(password)) {
-   //     $("#help-password").text("비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자").removeClass("is-safe").addClass("is-danger")
-   //     $("#input-password").focus()
-   //     return
-   // } else {
-   //     $("#help-password").text("사용할 수 있는 비밀번호입니다.").removeClass("is-danger").addClass("is-success")
-   // }
-   // if (password2 == "") {
-   //     $("#help-password2").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
-   //     $("#input-password2").focus()
-   //     return;
-   // } else if (password2 != password) {
-   //     $("#help-password2").text("비밀번호가 일치하지 않습니다.").removeClass("is-safe").addClass("is-danger")
-   //     $("#input-password2").focus()
-   //     return;
-   // } else {
-   //     $("#help-password2").text("비밀번호가 일치합니다.").removeClass("is-danger").addClass("is-success")
-   // }
+    // 아이디 공백체크
+    if (id == ""){
+        $("#help_id").text("이메일을 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#input_id").focus()
+        return;
+    } else{
+        $("#help_id").remove()
+    }
+
+    // 아이디 이메일형식 체크
+    if (checkEmail.test(id) == false) {
+//        $("#help_id").text("이메일을 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        alert('아이디가 이메일 형식이 아닙니다.')
+        return;
+    }
+
+    // 닉네임 공백 체크
+    if (nickname == ""){
+        $("#help_nickname").text("닉네임을 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#nickname").focus()
+        return;
+    } else{
+        $("#help_nickname").remove()
+    }
+
+    // 비밀번호 공백 체크
+    if (password == "") {
+        $("#help_password").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#input_password").focus()
+        return;
+    } else{
+        $("#help_password").remove()
+    }
+
+    // 비밀번호 확인 일치 체크
+    if (password != password2) {
+        $("#help_password2").text("비밀번호가 일치하지 않습니다.").removeClass("is-safe").addClass("is-danger")
+        $("#confirm_password").focus()
+        return;
+    } else  {
+        $("#help_password2").remove()
+    }
+
+    // 비밀번호 형식 체크
+    if (checkPassword.test(password) == false) {
+        alert('비밀번호는 영문과 숫자 조합 8자 이상이어야 합니다.')
+        return;
+    }
+
+
+
     $.ajax({
         type: "POST",
         url: "/register",
