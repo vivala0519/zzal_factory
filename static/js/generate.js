f10a185bef580d302405dd527fb53b55d70756d2
Dzeko <vivala0519@naver.com>
2022-01-12 14:55:53 +0900

    이미지 폰트 미선택 오류 수정

--- static/js/generate.js
+++ static/js/generate.js
@@ -6,6 +6,15 @@ function generate() {
     let img = "";
     console.log(zzal);
     console.log(color);
+
+    if (zzal.trim()==="이미지 선택짤" || color.trim() === "폰트 색상") {
+        alert("이미지와 폰트색상을 모두 선택해주세요.")
+        return;
+    }
+    if (text.trim() === "") {
+        alert("문구를 입력해주세요.")
+        return;
+    }
     if (zzal==="묻고 더블로 가!") {
         img = "double";
         console.log("체크");
