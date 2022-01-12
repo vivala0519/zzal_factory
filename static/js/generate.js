0e32ad57ee96b812cc645b92faea3588fdb8cfe3
HyunwooKim <hyunwoo.developer@gmail.com>
2022-01-12 14:59:20 +0900

    신사답게 행동해 추가

--- static/js/generate.js
+++ static/js/generate.js
@@ -6,11 +6,14 @@ function generate() {
     let img = "";
     console.log(zzal);
     console.log(color);
+
     if (zzal==="묻고 더블로 가!") {
         img = "double";
         console.log("체크");
     } else if(zzal === "한 끗인데 5억을 태워?"){
         img = "burning";
+    } else if(zzal === "신사답게 행동해") {
+        img = "gentleman";
     }
 
 
